366b69fcf8a43989ebad8c9a55225b0405845ed9
Christian Ost <christian.ost@typeform.com>
2018-11-07 20:47:36 +0100

    first draft

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+
+import ProductSelection from './product-selection'
+import ShoppingCart from './shopping-cart'
+
+class App extends PureComponent {
+  render () {
+    return <p>Go to our normal checkout</p>
+  }
+}
+
+export default App
---
+++ src/components/product-selection.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const ProductSelection = () => <p>Select your items of interest</p>
+
+export default ProductSelection
